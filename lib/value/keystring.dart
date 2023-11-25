06cd2f13d2be7263562b18214a1dd43ad27d61a3
LAI-PC <ljohnnguyen.2711@gmail.com>
2023-11-25 16:59:01 +0700

    update

--- lib/value/keystring.dart
+++ lib/value/keystring.dart
@@ -138,22 +138,24 @@ class Keystring {
   static const String COMPANY_ADDRESS = 'Company_Address';
   static const String COMPANY_FOllOW = 'Company_Follow';
   static const String FOllOWING = 'Following';
-  static const String CONTENT_DIALOG_UNFOLLOW = 'Content_Dialog_Unfollow'; 
-  static const String UNFOLLOW = 'Unfollow'; 
+  static const String CONTENT_DIALOG_UNFOLLOW = 'Content_Dialog_Unfollow';
+  static const String UNFOLLOW = 'Unfollow';
   static const String SEND_OTP = 'Send_OTP';
   static const String Confirm_OTP = 'Account_Authentication';
-  static const String OTP_Mail = 'Enter_the_verification_code_sent_to_your_email';
+  static const String OTP_Mail =
+      'Enter_the_verification_code_sent_to_your_email';
   static const String Get_OTP = 'Get_OTP';
   static const String Get_OTP_Fail = 'Email_is_not_exist';
   static const String ForgotPass = 'Forget_Password';
   static const String OTP_fail = 'OTP_fail';
   static const String Resend_OTP = 'Resend_OTP';
   static const String password_fail = 'Password_fail';
-  static const String  Changepass = 'Change_Password';
+  static const String Changepass = 'Change_Password';
   static const String NewPass = 'New_Password';
   static const String Confirmpass = 'Confirm_Password';
   static const String ConfirmCheckbox = 'I_agree_to_change_my_password.';
-  static const String Changepass_SuccessNotfication = 'Password_changed_successfully!';
+  static const String Changepass_SuccessNotfication =
+      'Password_changed_successfully!';
   static const String Changepass_FailNotification = 'Passwords_do_not_match!';
   static const String One_month = 'one_month';
   static const String PAYMENT = 'Payment';
@@ -167,5 +169,4 @@ class Keystring {
   static const String Order = 'Order';
   static const String Payment_status1 = 'Payment_done';
   static const String Payment_status2 = 'You_will';
-  
 }

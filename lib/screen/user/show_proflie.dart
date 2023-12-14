3985f35bcb7f1e72a03662a889ad5963d6f75580
xuananh2003 <124057490+xuananh2003@users.noreply.github.com>
2023-12-14 13:10:10 +0700

    Update show_proflie.dart

--- lib/screen/user/show_proflie.dart
+++ lib/screen/user/show_proflie.dart
@@ -1,45 +1,38 @@
-import 'dart:developer';
-import 'dart:io';
-
-import 'package:dropdown_button2/dropdown_button2.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:jobhunt_ftl/component/app_button.dart';
 import 'package:jobhunt_ftl/component/border_frame.dart';
-import 'package:jobhunt_ftl/component/date_dialog.dart';
-import 'package:jobhunt_ftl/component/loader_overlay.dart';
-import 'package:jobhunt_ftl/model/address.dart';
+import 'package:jobhunt_ftl/model/userprofile.dart';
 import 'package:jobhunt_ftl/value/keystring.dart';
-
-import '../../blocs/app_controller.dart';
-import '../../blocs/app_event.dart';
 import '../../blocs/app_riverpod_object.dart';
 import '../../blocs/app_riverpod_void.dart';
 import '../../component/edittext.dart';
 import '../../component/outline_text.dart';
 import '../../value/style.dart';
-import '../home.dart';
 
-class ShowProfileScreen extends ConsumerStatefulWidget {
-  const ShowProfileScreen({super.key});
+class ShowProfileScreen extends ConsumerWidget {
+  ShowProfileScreen({super.key, required this.profile});
+  final UserProfileDetail profile;
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _ShowProfileScreen();
-}
-
-class _ShowProfileScreen extends ConsumerState<ShowProfileScreen> {
-  @override
-  Widget build(BuildContext context) {
-    final profile = ref.watch(userProfileProvider);
-
-    final avatarProfile = profile?.avatarUrl ?? '';
+  Widget build(BuildContext context, WidgetRef ref) {
+    final avatarProfile = profile.avatarUrl ?? '';
     final recommendProfile = ref.watch(candidateRecommendProvider);
     final listJob = recommendProfile?.job?.split(',');
     final listEducation = recommendProfile?.education;
+
+    String showGender(String? gender) {
+      switch (gender) {
+        case 'Male':
+          return Keystring.MALE.tr;
+        case 'Female':
+          return Keystring.FEMALE.tr;
+        case 'Other':
+          return Keystring.OTHER.tr;
+        default:
+          return Keystring.NO_DATA.tr;
+      }
+    }
 
     return Container(
       decoration: BoxDecoration(
@@ -77,11 +70,12 @@ class _ShowProfileScreen extends ConsumerState<ShowProfileScreen> {
                   ),
                   SizedBox(width: 32),
                   Expanded(
-                      child: AppOutlineText(
-                        text: profile?.fullName ?? '',
-                        fontSize: 22,
-                        strokeWidth: 2,
-                      ),),
+                    child: AppOutlineText(
+                      text: profile.fullName ?? '',
+                      fontSize: 22,
+                      strokeWidth: 2,
+                    ),
+                  ),
                   SizedBox(width: 24),
                 ],
               ),
@@ -89,52 +83,60 @@ class _ShowProfileScreen extends ConsumerState<ShowProfileScreen> {
               EditTextForm(
                 onChanged: (value) => (),
                 label: Keystring.BIRTHDAY.tr,
-                content:
-                profile?.birthday ?? '',
+                content: profile.birthday ?? '',
                 readOnly: true,
               ),
               SizedBox(height: 24),
               EditTextForm(
                 onChanged: (value) => (),
                 label: Keystring.GENDER.tr,
-                content:
-                profile?.email ?? ref.watch(emailProfileProvider) ?? '',
+                content: showGender(recommendProfile?.gender),
                 readOnly: true,
               ),
               SizedBox(height: 24),
               AppBorderFrame(
                   labelText: Keystring.ADDRESS.tr,
-                  child: Text('${getWardName(profile!.address!.substring(profile!.address!.indexOf(',') + 1,
-                      profile!.address!.indexOf(',', profile!.address!.indexOf(',') + 1)), ref)}, '
-                      '${getDistrictName(profile!.address!.substring(
-                      profile!.address!.lastIndexOf(',', profile!.address!.lastIndexOf(',') - 1) + 1,
-                      profile!.address!.lastIndexOf(',')), ref)},\n'
-                      '${getProvinceName(profile!.address!.substring(profile.address!.lastIndexOf(',') + 1), ref)}',style: textNormal,)
-              ),
+                  child: Text(
+                    '${getWardName(profile.address!.substring(profile.address!.indexOf(',') + 1, profile.address!.indexOf(',', profile.address!.indexOf(',') + 1)), ref)}, '
+                        '${getDistrictName(profile.address!.substring(profile.address!.lastIndexOf(',', profile.address!.lastIndexOf(',') - 1) + 1, profile.address!.lastIndexOf(',')), ref)},\n'
+                        '${getProvinceName(profile.address!.substring(profile.address!.lastIndexOf(',') + 1), ref)}',
+                    style: textNormal,
+                  )),
               SizedBox(height: 24),
               AppBorderFrame(
-                labelText: Keystring.WANT_JOB.tr,
-                child: ListView.builder(
-                  physics: NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemBuilder: (_, index) {
-                    return Card(
-                      shadowColor: Colors.grey,
-                      margin: EdgeInsets.symmetric(
-                          horizontal: 8, vertical: 4),
-                      elevation: 2,
-                      child: ListTile(
-                        title: Text(
-                          listJob?[index] ?? '',
-                          overflow: TextOverflow.fade,
-                          maxLines: 2,
+                  labelText: Keystring.WANT_JOB.tr,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(left: 8),
+                        child: Text(
+                          '${Keystring.HAS.tr} ${recommendProfile?.yearExperience ?? 0} ${Keystring.Year_Experience.tr.toLowerCase()}',
+                          style: textNormal,
                         ),
                       ),
-                    );
-                  },
-                  itemCount: listJob?.length,
-                ),
-              ),
+                      ListView.builder(
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemBuilder: (_, index) {
+                          return Card(
+                            shadowColor: Colors.grey,
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 8, vertical: 4),
+                            elevation: 2,
+                            child: ListTile(
+                              title: Text(
+                                listJob?[index] ?? '',
+                                overflow: TextOverflow.fade,
+                                maxLines: 2,
+                              ),
+                            ),
+                          );
+                        },
+                        itemCount: listJob?.length,
+                      ),
+                    ],
+                  )),
               SizedBox(height: 24),
               AppBorderFrame(
                 labelText: Keystring.EDUCATION.tr,
@@ -144,15 +146,14 @@ class _ShowProfileScreen extends ConsumerState<ShowProfileScreen> {
                   itemBuilder: (_, index) {
                     return Card(
                       shadowColor: Colors.grey,
-                      margin: EdgeInsets.symmetric(
-                          horizontal: 8, vertical: 4),
+                      margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
                       elevation: 2,
                       child: ListTile(
                         title: Text(
-                            (Get.locale!.languageCode == 'en'
-                                ? listEducation![index].title_en
-                                : listEducation![index].title) ??
-                                '',
+                          (Get.locale!.languageCode == 'en'
+                              ? listEducation![index].title_en
+                              : listEducation![index].title) ??
+                              '',
                           overflow: TextOverflow.fade,
                           maxLines: 2,
                         ),
